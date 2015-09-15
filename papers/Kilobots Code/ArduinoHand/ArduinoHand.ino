b5b88ad88667c123839e9d5a523bd91a139a4111
shivashahrokhi <shiva.shahrokhi@gmail.com>
2015-09-14 19:21:58 -0500

    Matlab Code for hand covariance control and mathematica

---
+++ papers/Kilobots Code/ArduinoHand/ArduinoHand.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once
+  //pin 7 is the left light
+  //pin 5 is the right light
+  //pin 4 is the down light
+  //pin 3 is the up light
+  //put wanted pin as an output here.
+  //hit upload
+  int leftLight = 7;
+  int rightLight = 5;
+  int downLight = 4;
+  int upLight = 3;
+  pinMode(7, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(13,HIGH);
+
+}
