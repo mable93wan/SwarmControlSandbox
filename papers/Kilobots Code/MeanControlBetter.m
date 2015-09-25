9ba5002a8831246b6abe031b5ee0e6a110fe2f40
shivashahrokhi <shiva.shahrokhi@gmail.com>
2015-09-25 15:40:53 -0500

    Mean Control new

--- papers/Kilobots Code/MeanControlBetter.m
+++ papers/Kilobots Code/MeanControlBetter.m
@@ -3,31 +3,53 @@
 %%% Shahrokhi and Aaron T. Becker @ University of Houston, Robotic Swarm
 %%% Control Lab.
 
-function HandCov()
+function MeanControlBetter()
+
+% Using Arduino for our lamps, this is how we define arduino in Matlab:
+a = arduino('/dev/tty.usbmodem1411','uno');
 %Define webcam --the input may be 1 or 2 depending on which webcam of your laptop
 %is the default webcam.
 cam = webcam(2);
 global q goalX
+
+% We have 8 Relays.
+%west
+RELAY1 = 7;
+% northwest
+RELAY2 = 6;
+%north
+RELAY3 = 3;
+% northeast
+RELAY4 = 0;
+%east
+RELAY5 = 5;
+%southeast
+RELAY6 = 1;
+%south
+RELAY7 = 4;
+%southwest
+RELAY8 = 2;
 % this is the mean y goal
 goalYM = 434;
 %this is the mean x goal
 goalXM = 600;
 
-%These are the goal covariance matrices.
-goalC1 = [18000 -12000; -12000 18000];
-goalC2 = [18000 12000; 12000 18000];
-%Goal Covariances.
-goal1x = goalC1(1,2);
-goal2x = goalC2(1,2);
+epsilon = 10;
+
+%These are the goal X.
+
+goal1x = 800;
+goal2x = 600;
 goalX = goal1x;
 t0 = tic;
 q = zeros(1,2);
 
 success = false;
+again = true;
 
 tHandle = timer('TimerFcn',...
     {@sqWave_callback_fcn, goal1x, goal2x,t0}, ...
-    'Period' , 200, 'TasksToExecute' , 4, 'ExecutionMode', 'fixedDelay');
+    'Period' , 100, 'TasksToExecute' , 4, 'ExecutionMode', 'fixedDelay');
  
 start(tHandle);
 
@@ -35,7 +57,10 @@ start(tHandle);
 drawTime=[goalX,0];
 while success == false
 
-    
+    if again== true
+        writeDigitalPin(a, RELAY3, 0);
+    pause (1);
+    end 
     % Read in a webcam snapshot.
     rgbIm = snapshot(cam);
     %imwrite(rgbIm,'FailImage6.png');
@@ -73,15 +98,16 @@ BW = (I(:,:,1) >= channel1Min ) & (I(:,:,1) <= channel1Max) & ...
     imshow(originalImage);
     %h = viscircles(centers,radii);
     [s, l] = size(centers);
-    goalC = [18000 goalX; goalX 18000];
+    %goalC = [18000 goalX; goalX 18000];
     
     if isnan(M)== false 
         
     if s > 5 
+        again = false;
     hold on
     
     plot(M(1,1) , M(1,2),'*','Markersize',16,'color','red');
-    newDot = [C(1,2), toc(t0)];
+    newDot = [M(1,1), toc(t0)];
     drawTime = [drawTime;newDot];
     %Current Mean and Covariance Ellipse
     plot_gaussian_ellipsoid(M,C);
@@ -89,12 +115,35 @@ BW = (I(:,:,1) >= channel1Min ) & (I(:,:,1) <= channel1Max) & ...
     %plot_gaussian_ellipsoid([goalXM goalYM],goalC);
     %plot(centers(:,1),centers(:,2),'+','Markersize',16);
     %Goal X.
-    line([goalXM goalXM], ylim,'color','green','linewidth', 3.0);
+    line([goalX goalX], ylim,'color','green','linewidth', 3.0);
     hold off
+        if M(1,1) > goalX+epsilon
+        writeDigitalPin(a,RELAY3,1);
+        writeDigitalPin(a, RELAY1,0);
+        writeDigitalPin(a,RELAY5,1);
+        pause(1);
+       
+    else if M(1,1) < goalX-epsilon
+            writeDigitalPin(a,RELAY3,1);
+        writeDigitalPin(a, RELAY1,1);
+        writeDigitalPin(a,RELAY5,0);
+        pause(1);
+       
+        else 
+            writeDigitalPin(a,RELAY3,1);
+            writeDigitalPin(a, RELAY1,1);
+            writeDigitalPin(a,RELAY5,1);
+            pause(1);
+            again = true;
+        end
+        end
+    else
+        again = true;
     end
+    
     end
 
-    if(toc(t0) > 18)
+    if(toc(t0) > 400)
         success = true;
         figure(2)
        plot(q(:,1),q(:,2));
