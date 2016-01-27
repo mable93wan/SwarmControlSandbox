362e5d5fdefcd1df25b068edd058c882d0df764f
shivashahrokhi <shiva.shahrokhi@gmail.com>
2016-01-27 16:13:08 -0600

    BodyLength as a unit of covariance

--- papers/RSS2016/code/Data/FrictionPlot.m
+++ papers/RSS2016/code/Data/FrictionPlot.m
@@ -1,18 +1,18 @@
 load FrictionLess.txt;
 t1 = FrictionLess(:,1);
-C1 = FrictionLess(:,2);
+C1 = FrictionLess(:,2)*5;
 
 load FrictionFew.txt;
-C2 = FrictionFew(:,1);
+C2 = FrictionFew(:,1)*5;
 t2 = FrictionFew(:,2);
 
 load FrictionBigger.txt;
-C3 = FrictionBigger(:,1);
+C3 = FrictionBigger(:,1)*5;
 t3 = FrictionBigger(:,2);
 
 load FrictionData.txt;
 t4 = FrictionData(:,1);
-C4 = FrictionData(:,2);
+C4 = FrictionData(:,2)*5;
 
 plot(t1,C1, 'color','b')
 hold on
