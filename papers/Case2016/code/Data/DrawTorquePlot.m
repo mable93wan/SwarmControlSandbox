1d6cad96b98e4847da337f057779fdbb92a0d5b6
shivashahrokhi <shiva.shahrokhi@gmail.com>
2016-02-27 13:14:40 -0600

    New part2.

--- papers/Case2016/code/Data/DrawTorquePlot.m
+++ papers/Case2016/code/Data/DrawTorquePlot.m
@@ -1,4 +1,25 @@
-load TorqueStraight.txt;
-x = TorqueStraight(:,1);
-y = TorqueStraight(:,2);
+load Straight1.txt;
+x = Straight1(:,1);
+y = Straight1(:,2);
 plot(x,y)
+hold on
+load Straight2.txt;
+x2 = Straight2(:,1);
+y2 = Straight2(:,2);
+plot(x2,y2)
+
+load Straight3.txt;
+x3 = Straight3(:,1);
+y3 = Straight3(:,2);
+plot(x3,y3)
+
+load Straight4.txt;
+x4 = Straight4(:,1);
+y4 = Straight4(:,2);
+plot(x4,y4)
+
+load Straight5.txt;
+x5 = Straight5(:,1);
+y5 = Straight5(:,2);
+plot(x5,y5)
+hold off
